9545a70ee2086e5d953840cacb8f5475d69b74f4
Martin <m.hein090@gmail.com>
2022-09-07 13:41:12 +0100

    Created Log class

--- modules/Logger.py
+++ modules/Logger.py
@@ -1,7 +1,21 @@
+class Log:
+    def __init__(self, name, path):
+        self.__name = name
+        self.__path = path
+
+    def log(self):
+        pass
+
+    @property
+    def name(self):
+        return self.__name
 
 class Logger:
     def __init__(self):
-        pass
+        self.__logs = []
 
-    def log_operation(self, message, message_type):
-        print(message)
+    def log(self, log_name, message, message_type, ):
+        for log in self.__logs:
+            if log_name == log.name:
+                log.log()
+        # print(message)
